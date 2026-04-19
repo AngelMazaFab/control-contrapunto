6b0bd24288df1e0e0521ba0a83093ab0b6b7e299
AngelMazaFab <angelfabilam@gmail.com>
2026-04-18 20:20:22 -0600

    Primer commit: Inicialización de proyecto Spring Boot

---
+++ src/main/java/com/contrapunto/control_contrapunto/ControlContrapuntoApplication.java
@@ -0,0 +1,13 @@
+package com.contrapunto.control_contrapunto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlContrapuntoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlContrapuntoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contrapunto/control_contrapunto/ControlContrapuntoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contrapunto.control_contrapunto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlContrapuntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
